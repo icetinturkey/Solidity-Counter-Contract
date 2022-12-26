4784cfd3d422dc81180a438e1e59d57d11822455
ismail <41130134+icetinturkey@users.noreply.github.com>
2022-12-26 14:40:41 +0300

    Update counter.sol
    Added comment lines.

--- counter.sol
+++ counter.sol
@@ -4,15 +4,15 @@ pragma solidity >=0.7.0 <0.9.0;
 contract Counter {
     uint public sayac;
 
-    function yazdir() public view returns (uint) {
+    function yazdir() public view returns (uint) { //Sayaci ekrana yazdirir.
         return sayac;
     }
 
-    function artir() public {
+    function artir() public { //Sayaci 1 artirir.
         sayac += 1;
     }
 
-    function azalt() public {
+    function azalt() public { //Sayaci 1 azaltir. Sifir altina düserse hata verir.
         sayac -= 1;
     }
 }
