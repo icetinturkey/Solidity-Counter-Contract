453378b1079a8a9556b3531827a6a0e458c739dd
ismail <41130134+icetinturkey@users.noreply.github.com>
2022-12-26 14:37:18 +0300

    Update counter.sol
    Prepared.

--- counter.sol
+++ counter.sol
@@ -1,1 +1,18 @@
-test
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Counter {
+    uint public sayac;
+
+    function yazdir() public view returns (uint) {
+        return sayac;
+    }
+
+    function artir() public {
+        sayac += 1;
+    }
+
+    function azalt() public {
+        sayac -= 1;
+    }
+}
